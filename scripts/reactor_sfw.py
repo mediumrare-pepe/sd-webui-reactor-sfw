59945bb6824bc9d5648d00e2726bc716b76d25dd
Aleks <alex9025@gmail.com>
2025-06-03 12:20:32 -0400

    Update reactor_sfw.py

--- scripts/reactor_sfw.py
+++ scripts/reactor_sfw.py
@@ -11,8 +11,8 @@ def nsfw_image(img_path: str, model_path: str):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     with Image.open(img_path) as img:
         predict = pipeline("image-classification", model=model_path)
-        predict.model.to(device)Add commentMore actions
+        predict.model.to(device)
         result = predict(img)
         score = result[0]["score"]
         print(f"NSFW Score = {score}")
-        return False
+        return True if score > SCORE else False

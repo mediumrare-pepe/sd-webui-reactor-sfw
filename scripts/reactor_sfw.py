6bfc9c8e09f92e194051079be2183d4a5ad0ffba
Aleks <alex9025@gmail.com>
2025-06-03 12:08:05 -0400

    Update reactor_sfw.py

--- scripts/reactor_sfw.py
+++ scripts/reactor_sfw.py
@@ -8,4 +8,11 @@ SCORE = 0.965 # 0.965 and less - is safety content
 logging.getLogger('transformers').setLevel(logging.ERROR)
 
 def nsfw_image(img_path: str, model_path: str):
-    return False
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    with Image.open(img_path) as img:
+        predict = pipeline("image-classification", model=model_path)
+        predict.model.to(device)Add commentMore actions
+        result = predict(img)
+        score = result[0]["score"]
+        print(f"NSFW Score = {score}")
+        return False
